1af1a401ee922716eb68fb6e972bd6c3fbd4a8b4
liu-ge-ge <2212722889@qq.com>
2023-05-27 16:22:09 +0800

    feat: less和环境变量引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import path from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    open: true,
+    port: 8888,
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        // additionalData: `@import "${path.resolve(__dirname, 'src/style/index.less')}`,
+        modifyVars: {
+          hack: `true; @import (reference) "${path.resolve('src/styles/index.less')}"`,
+        },
+      },
+    },
+  },
+  plugins: [vue(), createHtmlPlugin()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        keep_infinity: true,
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name]_[hash:10][extname]',
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
